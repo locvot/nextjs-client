fa008c35f4f5e6c9b2a104671dd08ee297a9fe07
locvot <vol6921@gmail.com>
2025-08-25 14:24:32 +0700

    UI Setting Template

--- src/schemaValidations/account.schema.ts
+++ src/schemaValidations/account.schema.ts
@@ -9,8 +9,6 @@ export const AccountSchema = z.object({
   avatar: z.string().nullable()
 })
 
-export type AccountType = z.TypeOf<typeof AccountSchema>
-
 export const AccountRes = z
   .object({
     data: AccountSchema,
@@ -18,4 +16,31 @@ export const AccountRes = z
   })
   .strict()
 
+export const ChangePasswordBody = z
+  .object({
+    oldPassword: z.string().min(6).max(100),
+    password: z.string().min(6).max(100),
+    confirmPassword: z.string().min(6).max(100)
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Mật khẩu mới không khớp',
+        path: ['confirmPassword']
+      })
+    }
+  })
+
+export const UpdateMeBody = z
+  .object({
+    name: z.string().trim().min(2).max(256),
+    avatar: z.string().url().optional()
+  })
+  .strict()
+
+export type AccountType = z.TypeOf<typeof AccountSchema>
 export type AccountResType = z.TypeOf<typeof AccountRes>
+export type ChangePasswordBodyType = z.TypeOf<typeof ChangePasswordBody>
+export type UpdateMeBodyType = z.TypeOf<typeof UpdateMeBody>
