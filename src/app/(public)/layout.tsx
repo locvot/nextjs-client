dbcb21d794d999491ef60a2997e5579fb899bbaf
locvot <vol6921@gmail.com>
2025-08-26 23:59:12 +0700

    get dish list at home page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import { Menu, Package2 } from 'lucide-react'
-import NavItems from '@/app/(public)/(auth)/nav-items'
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import DarkModeToggle from '@/components/dark-mode-toggle'
+import NavItems from '@/app/(public)/nav-items'
 
 export default function Layout({
   children
@@ -12,7 +12,7 @@ export default function Layout({
 }>) {
   return (
     <div className='flex min-h-screen w-full flex-col relative'>
-      <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
+      <header className='sticky z-20 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
         <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
           <Link href='#' className='flex items-center gap-2 text-lg font-semibold md:text-base'>
             <Package2 className='h-6 w-6' />
@@ -28,6 +28,10 @@ export default function Layout({
             </Button>
           </SheetTrigger>
           <SheetContent side='left'>
+            <SheetHeader className='sr-only'>
+              <SheetTitle />
+              <SheetDescription />
+            </SheetHeader>
             <nav className='grid gap-6 text-lg font-medium'>
               <Link href='#' className='flex items-center gap-2 text-lg font-semibold'>
                 <Package2 className='h-6 w-6' />
@@ -42,7 +46,7 @@ export default function Layout({
           <DarkModeToggle />
         </div>
       </header>
-      <main className='flex flex-1 flex-col justify-center gap-4 p-4 md:gap-8 md:p-8'>{children}</main>
+      <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8'>{children}</main>
     </div>
   )
 }
