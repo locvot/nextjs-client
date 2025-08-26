61e375e1edbe40e477568a04122115b9f549805a
locvot <vol6921@gmail.com>
2025-08-26 21:57:14 +0700

    declare api for tables

---
+++ src/queries/useTable.tsx
@@ -0,0 +1,56 @@
+import tableApiRequest from '@/apiRequests/table'
+import { UpdateTableBodyType } from '@/schemaValidations/table.schema'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { number } from 'zod'
+
+export const useTableListQuery = () => {
+  return useQuery({
+    queryKey: ['tables'],
+    queryFn: tableApiRequest.list
+  })
+}
+
+export const useGetTabQuery = ({ id, enabled }: { id: number; enabled: boolean }) => {
+  return useQuery({
+    queryKey: ['tables', id],
+    queryFn: () => tableApiRequest.getTable(id),
+    enabled
+  })
+}
+
+export const useAddTableMutation = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: tableApiRequest.add,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tables'] })
+    }
+  })
+}
+
+export const useUpdateTableMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: ({ id, ...body }: UpdateTableBodyType & { id: number }) => tableApiRequest.updateTable(id, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tables'],
+        exact: true
+      })
+    }
+  })
+}
+
+export const useDeleteTableMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: tableApiRequest.deleteTable,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tables']
+      })
+    }
+  })
+}
