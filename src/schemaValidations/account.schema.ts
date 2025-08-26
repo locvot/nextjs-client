57603bd5e63ef68e892fca2872b0a1d38c3a2c82
locvot <vol6921@gmail.com>
2025-08-26 17:35:57 +0700

    UI accounts

--- src/schemaValidations/account.schema.ts
+++ src/schemaValidations/account.schema.ts
@@ -16,6 +16,11 @@ export const AccountRes = z
   })
   .strict()
 
+export const AccountListRes = z.object({
+  data: z.array(AccountSchema),
+  message: z.string()
+})
+
 export const ChangePasswordBody = z
   .object({
     oldPassword: z.string().min(6).max(100),
@@ -40,7 +45,58 @@ export const UpdateMeBody = z
   })
   .strict()
 
+export const CreateEmployeeAccountBody = z
+  .object({
+    name: z.string().trim().min(2).max(256),
+    email: z.string().email(),
+    avatar: z.string().url().optional(),
+    password: z.string().min(6).max(100),
+    confirmPassword: z.string().min(6).max(100)
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Mật khẩu không khớp',
+        path: ['confirmPassword']
+      })
+    }
+  })
+
+export const UpdateEmployeeAccountBody = z
+  .object({
+    name: z.string().trim().min(2).max(256),
+    email: z.string().email(),
+    avatar: z.string().url().optional(),
+    changePassword: z.boolean().optional(),
+    password: z.string().min(6).max(100).optional(),
+    confirmPassword: z.string().min(6).max(100).optional(),
+    role: z.enum([Role.Owner, Role.Employee]).default(Role.Employee).optional()
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password, changePassword }, ctx) => {
+    if (changePassword) {
+      if (!password || !confirmPassword) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Hãy nhập mật khẩu mới và xác nhận mật khẩu mới',
+          path: ['changePassword']
+        })
+      } else if (confirmPassword !== password) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Mật khẩu không khớp',
+          path: ['confirmPassword']
+        })
+      }
+    }
+  })
+
 export type AccountType = z.TypeOf<typeof AccountSchema>
 export type AccountResType = z.TypeOf<typeof AccountRes>
 export type ChangePasswordBodyType = z.TypeOf<typeof ChangePasswordBody>
 export type UpdateMeBodyType = z.TypeOf<typeof UpdateMeBody>
+export type AccountListResType = z.TypeOf<typeof AccountListRes>
+export type CreateEmployeeAccountBodyType = z.TypeOf<typeof CreateEmployeeAccountBody>
+export type UpdateEmployeeAccountBodyType = z.TypeOf<typeof UpdateEmployeeAccountBody>
